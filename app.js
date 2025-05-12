f1fd661b109ae817b4d7be1e1654d1f54bef5b28
Hrishikesh Shinde <rishikeshs2003@gmail.com>
2025-05-12 13:35:38 +0530

    database updated

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 
 const Listing = require("./model/listing.js")
 
+// const Listing = require("./views/listings/")
 main()
  .then(() => {
         console.log("connected to DB");
@@ -19,6 +21,9 @@ async function main () {
     await mongoose.connect('mongodb://127.0.0.1:27017/rentwala');
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // if folder is "views"
+
 
 app.get("/" , (req, res) => {
     res.send("this is a Root");
@@ -26,20 +31,35 @@ app.get("/" , (req, res) => {
 
 
 
-app.get("/testlisting" , async (req, res) => {
+app.get("/listings", async (req ,res) => {
+
+    const allListings = await Listing.find({});
+    // res.render("/views/index", { allListings });
+    // console.log("Rendering index view with data:", allListings);
+res.render("index", { allListings });
 
 
-    let sampleListing = new Listing({
-        title: "my new home",
-        descriptin: "by the beach",
-        price: 2000,
-        locatin: "maharashtra ",
-        country: "India",
-    });
-    await sampleListing.save();
-    console.log("sample was save");
-    res.send("sucessful");
-})
+
+
+});
+
+
+
+
+// app.get("/testlisting" , async (req, res) => {
+
+
+//     let sampleListing = new Listing({
+//         title: "my new home",
+//         descriptin: "by the beach",
+//         price: 2000,
+//         locatin: "maharashtra ",
+//         country: "India",
+//     });
+//     await sampleListing.save();
+//     console.log("sample was save");
+//     res.send("sucessful");
+// })
 
 
 app.listen(8080, () => {
